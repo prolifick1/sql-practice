3cf49bc94903863f71de951526c4fe25ba90ed47
Luis Manzo <manzosk3@gmail.com>
2021-03-16 18:21:38 -0400

    adds answers to Portilla subquery lesson 061

---
+++ portilla-sql-bootcamp/061-subquery.sql
@@ -0,0 +1,12 @@
+-- SQL Lesson 1: SELECT queries 101 
+-- (Link: https://sqlbolt.com/lesson/select_queries_introduction)
+
+-- Q1: find films whose rental rate is higher than the average rental rate 
+SELECT avg(rental_rate) FROM film;  #2.98
+SELECT title, rental_rate FROM film WHERE rental_rate > 2.98;
+
+-- Q2:  find films whose rentel rate is higher than the average rental rate
+SELECT title, rental_rate FROM film WHERE rental_rate > (SELECT AVG(rental_rate) FROM film);
+
+
+-- Q3: Get films (by id) that have been returned between may 29th, 2005 and may 30th, 2005
