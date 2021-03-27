f0ec17f459432c3cea661552f18e4594f88e243e
Luis Manzo <manzosk3@gmail.com>
2021-03-27 00:20:02 -0400

    adds Portilla selfjoin lesson

---
+++ portilla-sql-bootcamp/062-selfjoin.sql
@@ -0,0 +1,4 @@
+-- SQL Lesson: SELFJOIN
+
+-- Q1 retrieve all customers whose last_name matches the first_name of another customer
+SELECT a.first_name, a.last_name, b.first_name, b.last_name FROM customer AS a, customer AS b WHERE a.first_name = b.last_name;
