6b40b803a7dc479cf340d779e9f7babcd0a9578a
Luis Manzo <manzosk3@gmail.com>
2021-04-17 16:30:28 -0400

    adds sqlbolt 02-queries-with-constraints

--- jeffreyJames/04-dowExtractCASE.sql
+++ jeffreyJames/04-dowExtractCASE.sql
@@ -5,7 +5,17 @@ SELECT extract(dow FROM p.payment_date) dow, sum(p.amount)::money  FROM payment 
 
 
 -- Q2: use a case statement to make dow human readable (remember dow start val)
-SELECT *, CASE WHEN t.dow = 0 THEN 'Sunday' WHEN t.dow = 1 THEN 'Monday' WHEN t.dow = 2 THEN 'Tuesday' WHEN t.dow = 3 THEN 'Wednesday' WHEN t.dow = 4 THEN 'Thursday' WHEN t.dow = 5 THEN 'Friday' WHEN t.dow = 6 THEN 'Saturday' END FROM (SELECT extract(dow FROM p.payment_date) dow, SUM(p.amount)::money FROM payment p GROUP BY dow ORDER BY dow) t;
+SELECT *,
+CASE
+  WHEN t.dow = 0 THEN 'Sunday'
+  WHEN t.dow = 1 THEN 'Monday'
+  WHEN t.dow = 2 THEN 'Tuesday'
+  WHEN t.dow = 3 THEN 'Wednesday'
+  WHEN t.dow = 4 THEN 'Thursday'
+  WHEN t.dow = 5 THEN 'Friday'
+  WHEN t.dow = 6 THEN 'Saturday'
+END
+FROM (SELECT extract(dow FROM p.payment_date) dow, SUM(p.amount)::money FROM payment p GROUP BY dow ORDER BY dow) t;
 
 --Q3: Use case statement to deal with count with at least 20 orders (do not use HAVING clause)
 
